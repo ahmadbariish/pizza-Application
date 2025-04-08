a471fa4e2f9475930991788b59609dcf31d35d20
Ahmad Barish <ahmad.bariish@gmail.com>
2025-04-08 19:24:06 +0300

    create Flolder in file lib

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app());
+}
+
+class app extends StatelessWidget {
+  const app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: Text('h'),
+        ),
+      ),
+    );
+  }
+}
